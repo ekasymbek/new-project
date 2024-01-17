7442cc482db90f478a28158abbcb1b708442e2d4
erkin <erkin.kasymbek@gmail.com>
2024-01-17 19:50:20 +0600

    added function

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const array = ["apple", "orange", "grape"];
 console.log(array);
 console.log(array.length);
 const array2 = [];
+function showItems(arr) {
+    arr.forEach(item => console.log(item))
+}
+showItems(array);
